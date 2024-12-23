f34e23c91eba66ad0ed7d4e918a2b6af4f3f48bf
Simon Ament <simon.ament@student.hpi.uni-potsdam.de>
2024-12-23 12:13:27 +0100

    fix: correct file sorting function

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,12 +1,38 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
+import { FileNode } from "./quartz/components/ExplorerNode"
+
+const fileSortFn = (a: FileNode, b: FileNode) => {
+  if (a.file && b.file) {
+    // sort by filename
+    return a.name.localeCompare(b.name, undefined, {
+      numeric: true,
+      sensitivity: "base",
+    })
+  }
+  if (!a.file && !b.file) {
+    // sensitivity: "base": Only strings that differ in base letters compare as unequal. Examples: a ≠ b, a = á, a = A
+    // numeric: true: Whether numeric collation should be used, such that "1" < "2" < "10"
+    return a.displayName.localeCompare(b.displayName, undefined, {
+      numeric: true,
+      sensitivity: "base",
+    })
+  }
+  if (a.file && !b.file) {
+    return 1
+  } else {
+    return -1
+  }
+}
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
   afterBody: [],
-  footer: Component.CustomFooter(),
+  footer: Component.CustomFooter({
+    sortFn: fileSortFn,
+  }),
 }
 
 // components for pages that display a single page (e.g. a single note)
@@ -22,12 +48,17 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(Component.Explorer({
+      sortFn: fileSortFn,
+    })),
   ],
   right: [
+    Component.MobileOnly(Component.Explorer({
+      sortFn: fileSortFn,
+    })),
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
+    Component.DesktopOnly(Component.Backlinks()),
   ],
 }
 
@@ -39,7 +70,9 @@ export const defaultListPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(Component.Explorer({
+      sortFn: fileSortFn,
+    })),
   ],
   right: [],
 }
